c94614062dcebe4dded28e9568a06994550e5efd
Ahmad Alqaisi <ahmedhamed1312@gmail.com>
2022-07-23 14:40:49 +0300

    adding mobile menu

---
+++ script.js
@@ -0,0 +1,27 @@
+const menuIcon=document.querySelector(".menuIcon");
+const overlay=document.querySelector(".overlay");
+
+menuIcon.addEventListener("click",function(evt){
+    if(overlay.classList.contains("hidden"))
+        overlay.classList.remove("hidden");
+    console.log(evt.target.alt)    
+    toggleMenu(evt.target.alt)
+});
+
+function toggleMenu(menuStatus){
+    
+    overlay.classList.toggle("hide");
+    menuIcon.setAttribute("alt",menuStatus=="openMenu"?"closeMenu":"openMenu");
+    document.body.style.overflowY=menuStatus=="openMenu"?"hidden":"auto";
+    if(menuStatus=="openMenu")
+        return menuIcon.setAttribute("src","images/icon-close.svg");
+    menuIcon.setAttribute("src","images/icon-hamburger.svg");
+}
+
+function checkViewSize(query){
+    if (query.matches && menuIcon.alt=="closeMenu"){ 
+        toggleMenu("closeMenu")
+    }
+}
+const query=window.matchMedia("(min-width:1076px)");
+query.addListener(checkViewSize);
